c878b1bad9ea17743a14318d1478f821ed0aa041
Tim Flood <tflood9@gmail.com>
2021-05-05 11:10:35 -0400

    created button component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+class OneTimeButton extends React.Component {
+  state = {
+    clicked: false,
+  };
+  handleClick = () => {
+    this.props.onClick();
+
+    this.setState({ clicked: true });
+  };
+
+  render() {
+    return (
+      <button onClick={this.handleClick} disabled={this.state.clicked}>
+        You can only click me once
+      </button>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <OneTimeButton onClick={() => alert("hi")} />,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
